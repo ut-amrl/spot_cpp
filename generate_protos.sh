04dc585e8c8b50fb7e66bd3f2de0d8b7f5e60e47
Nathaniel <natnemenzo1@gmail.com>
2021-06-10 09:53:15 -0500

    generate protos correctly

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-pushd spot_comm
+protoc -I . --grpc_out=./include --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` \
+		bosdyn/api/*.proto \
+		bosdyn/api/docking/*.proto \
+		bosdyn/api/spot/*.proto \
+		bosdyn/api/spot_cam/*.proto \
+		bosdyn/api/mission/*.proto \
+		bosdyn/api/graph_nav/*.proto
 
-protoc -I . --grpc_out=./include --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` bosdyn/api/*.proto bosdyn/api/docking/*.proto bosdyn/api/spot/*.proto bosdyn/api/spot_cam/*.proto bosdyn/api/mission/*.proto bosdyn/api/graph_nav/*.proto
-protoc -I . --cpp_out=./include bosdyn/api/*.proto bosdyn/api/docking/*.proto bosdyn/api/spot/*.proto bosdyn/api/spot_cam/*.proto bosdyn/api/mission/*.proto bosdyn/api/graph_nav/*.proto
-
-popd
-
+protoc -I . --cpp_out=./include \
+		bosdyn/api/*.proto \
+		bosdyn/api/docking/*.proto \
+		bosdyn/api/spot/*.proto \
+		bosdyn/api/spot_cam/*.proto \
+		bosdyn/api/mission/*.proto \
+		bosdyn/api/graph_nav/*.proto
