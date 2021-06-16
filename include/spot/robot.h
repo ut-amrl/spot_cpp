93d4edc3c58d615ee95b74bd54dc25d1b6f19fa0
swathimannem <s.mannem1300@gmail.com>
2021-06-16 16:50:49 -0500

    trajectory

--- include/spot/robot.h
+++ include/spot/robot.h
@@ -23,7 +23,7 @@
 #include <map>
 #include <list>
 
-enum movementType {sit, stand, travel};
+enum movementType {sit, stand, travelBasic, travelTrajectory};
 
 // class that encapsulates robot state
 class State {
--- src/robot.cpp
+++ src/robot.cpp
@@ -125,7 +125,8 @@ void Robot::powerOn() {
     // _isOn = true;
 }
 
-bool Robot::move(movementType mType, double x, double y, double rot, double time, int64_t clockSkew, std::__cxx11::string timeSyncClockId){
+// move method for sitting and standing 
+bool Robot::move(movementType mType, std::__cxx11::string timeSyncClockId){
 	if (_robotCommandClientPtr == NULL){
 	        std::cout << "Need to setup" << std::endl; 
 	        throw 1;
@@ -140,7 +141,23 @@ bool Robot::move(movementType mType, double x, double y, double rot, double time
 		case stand:
 			command.mutable_synchronized_command()->mutable_mobility_command()->mutable_stand_request();
 			break;
-		case travel:
+	}
+
+	RobotCommandResponse robCommResp = _robotCommandClientPtr->robotCommand(*_leasePtr, command, timeSyncClockId);
+    return (robCommResp.status() == 1);
+}
+
+// move method for travelling
+bool Robot::move(movementType mType, double x, double y, double rot, double time, int64_t clockSkew, std::__cxx11::string timeSyncClockId){
+	if (_robotCommandClientPtr == NULL){
+	        std::cout << "Need to setup" << std::endl; 
+	        throw 1;
+	 } // TODO: change later 
+	
+	RobotCommand command;
+
+	switch (mType){
+		case travelBasic:
 			SE2VelocityCommand_Request se2VelocityCommand_Request;
 			se2VelocityCommand_Request.mutable_end_time()->CopyFrom(TimeUtil::NanosecondsToTimestamp(((TimeUtil::TimestampToNanoseconds(TimeUtil::GetCurrentTime()) + clockSkew) + time*1000000000)));
 			se2VelocityCommand_Request.set_se2_frame_name(BODY_FRAME_NAME);
@@ -154,10 +171,20 @@ bool Robot::move(movementType mType, double x, double y, double rot, double time
 			velocity.set_angular(rot);
 */
 //			se2VelocityCommand_Request->mutable_velocity()->copyFrom(velocity);
-
-			RobotCommand command2;
-			command2.mutable_synchronized_command()->mutable_mobility_command()->mutable_se2_velocity_request()->CopyFrom(se2VelocityCommand_Request);
+			command.mutable_synchronized_command()->mutable_mobility_command()->mutable_se2_velocity_request()->CopyFrom(se2VelocityCommand_Request);
 			break;
+        // case travelTrajectory:
+        //     command.mutable_synchronized_command()->mutable_mobility_command()->mutable_se2_trajectory_request()->mutable_end_time()->CopyFrom(TimeUtil::NanosecondsToTimestamp(((TimeUtil::TimestampToNanoseconds(TimeUtil::GetCurrentTime()) + clockSkew) + time*1000000000)));
+        //     command.mutable_synchronized_command()->mutable_mobility_command()->mutable_se2_trajectory_request()->set_se2_frame_name(BODY_FRAME_NAME);
+        //     SE2Trajectory trajectory;
+        //     trajectory.set_interpolation(POS_INTERP_LINEAR);
+        //     SE2TrajectoryPoint trajectoryPoint;
+        //     trajectoryPoint.mutable_pose()->mutable_position()->setx(0);
+        //     trajectoryPoint.mutable_pose()->mutable_position()->sety(0);
+        //     trajectoryPoint.mutable_pose()->set_angle(3.14/4);
+        //     trajectoryPoint.mutable_time_since_reference()->CopyFrom(TimeUtil::SecondsToDuration(3));
+        //     trajectory.mutable_points()->CopyFrom(trajectoryPoint);
+        //     command.mutable_synchronized_command()->mutable_mobility_command()->mutable_se2_trajectory_request()->mutable_trajectory()->CopyFrom(trajectory);
 	}
 
 	RobotCommandResponse robCommResp = _robotCommandClientPtr->robotCommand(*_leasePtr, command, timeSyncClockId);
