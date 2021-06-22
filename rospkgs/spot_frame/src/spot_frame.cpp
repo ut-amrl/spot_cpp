db4761479a9ea40c1cc5d5e6904b33055903d853
matkozlowski <mkozlowski@utexas.edu>
2021-06-22 11:16:00 -0500

    Added cmd line args to pkg

--- rospkgs/spot_frame/src/spot_frame.cpp
+++ rospkgs/spot_frame/src/spot_frame.cpp
@@ -1,7 +1,12 @@
 #include "spot_frame/spot_frame.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+	assert(argc == 3);
+
+	// get username and password
+	const std::string username = argv[1];
+	const std::string password = argv[2];
+
 	ros::init(argc, argv, "spot_frame_client");
 	ros::NodeHandle node_handle;
 
