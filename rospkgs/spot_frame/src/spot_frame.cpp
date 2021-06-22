c7667371206b2d4c244469a79fae7c8557d684a1
matkozlowski <mkozlowski@utexas.edu>
2021-06-22 14:25:38 -0500

    Display transforms of camera

--- rospkgs/spot_frame/src/spot_frame.cpp
+++ rospkgs/spot_frame/src/spot_frame.cpp
@@ -10,20 +10,22 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "spot_frame_client");
 	ros::NodeHandle node_handle;
 
-	TFBroadcastPR tfbr;
-	tfbr.setFrames("origin", "test");
-	geometry_msgs::Pose pose;
-	pose.position.x = 1;
-	pose.position.y = 1;
-	pose.position.z = 1;
-	pose.orientation.w = 1;
-	pose.orientation.x = 0;
-	pose.orientation.y = 0;
-	pose.orientation.z = 0;
-	while(true){
-		tfbr.receivePose(pose);
-		sleep(1);
-	}
+	// TFBroadcastPR tfbr;
+	// tfbr.setFrames("origin", "test");
+	// geometry_msgs::Pose pose;
+	// pose.position.x = 1;
+	// pose.position.y = 1;
+	// pose.position.z = 1;
+	// pose.orientation.w = 1;
+	// pose.orientation.x = 0;
+	// pose.orientation.y = 0;
+	// pose.orientation.z = 0;
+	// while(true){
+	// 	tfbr.receivePose(pose);
+	// 	sleep(1);
+	// }
+
+	std::vector<geometry_msgs::Pose> tfbrList;
 	
 
 	Robot spot("spot");
@@ -48,24 +50,39 @@ int main(int argc, char **argv) {
 	imgReq1.set_quality_percent(100);
 	imgReq1.set_image_format(bosdyn::api::Image_Format_FORMAT_RAW);
 	imgReqs.push_back(imgReq1);
+	while(true){
 	ImageResponse imgResp = spot.getImageClientPtr()->getImage(imgReqs).image_responses(0);
 	google::protobuf::Map<std::string, bosdyn::api::FrameTreeSnapshot::ParentEdge> frameMap = imgResp.shot().transforms_snapshot().child_to_parent_edge_map();
-	std::cout << "Image frames: " << std::endl;
 	for (google::protobuf::Map<std::string, bosdyn::api::FrameTreeSnapshot::ParentEdge>::const_iterator it=frameMap.begin(); it!=frameMap.end(); ++it){
-    	std::cout << "Parent-Child combo:" << std::endl;
-		std::cout << "Child Name: " << it->first << std::endl;
-		std::cout << "Parent Name: " << it->second.parent_frame_name() << std::endl << std::endl;
+		// std::cout << "Parent-Child combo:" << std::endl;
+		// std::cout << "Child Name: " << it->first << std::endl;
+		// std::cout << "Parent Name: " << it->second.parent_frame_name() << std::endl << std::endl;
+		if(!it->second.parent_frame_name().empty()){
+			TFBroadcastPR tfbr;
+			tfbr.setFrames(it->second.parent_frame_name(), it->first);
+			geometry_msgs::Pose pose;
+			pose.position.x = it->second.parent_tform_child().position().x();
+			pose.position.y = it->second.parent_tform_child().position().y();
+			pose.position.z = it->second.parent_tform_child().position().z();
+			pose.orientation.w = it->second.parent_tform_child().rotation().w();
+			pose.orientation.x = it->second.parent_tform_child().rotation().x();
+			pose.orientation.y = it->second.parent_tform_child().rotation().y();
+			pose.orientation.z = it->second.parent_tform_child().rotation().z();
+			tfbr.receivePose(pose);
+		}
+		
+	}
 	}
 	std::cout << std::endl << std::endl << std::endl;
 
-	std::cout << "Kinematic frames:" << std::endl;
-	RobotStateResponse rState = spot.getRobotStateClientPtr()->getRobotState();
-	frameMap = rState.robot_state().kinematic_state().transforms_snapshot().child_to_parent_edge_map();
-	for (google::protobuf::Map<std::string, bosdyn::api::FrameTreeSnapshot::ParentEdge>::const_iterator it=frameMap.begin(); it!=frameMap.end(); ++it){
-    	std::cout << "Parent-Child combo:" << std::endl;
-		std::cout << "Child Name: " << it->first << std::endl;
-		std::cout << "Parent Name: " << it->second.parent_frame_name() << std::endl << std::endl;
-	}
+	// std::cout << "Kinematic frames:" << std::endl;
+	// RobotStateResponse rState = spot.getRobotStateClientPtr()->getRobotState();
+	// frameMap = rState.robot_state().kinematic_state().transforms_snapshot().child_to_parent_edge_map();
+	// for (google::protobuf::Map<std::string, bosdyn::api::FrameTreeSnapshot::ParentEdge>::const_iterator it=frameMap.begin(); it!=frameMap.end(); ++it){
+    // 	std::cout << "Parent-Child combo:" << std::endl;
+	// 	std::cout << "Child Name: " << it->first << std::endl;
+	// 	std::cout << "Parent Name: " << it->second.parent_frame_name() << std::endl << std::endl;
+	// }
 
 	ros::spin();
 
