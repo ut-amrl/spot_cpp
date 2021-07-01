ff9c3c7415bea396547058b99ce00f9b5762b34b
nnemenzo9 <natnemenzo1@gmail.com>
2021-07-01 11:26:39 -0500

    moved math stuff to math.h, small updates to classes

--- include/spot/frame.h
+++ include/spot/frame.h
@@ -15,11 +15,6 @@
 #include "bosdyn/api/lease_service.grpc.pb.h"
 #include <spot/clients/base.h>
 
-using bosdyn::api::Vec2;
-using bosdyn::api::SE2Pose;
-using bosdyn::api::Vec3;
-using bosdyn::api::SE3Pose;
-
 enum gravAlignedFrame {
     ODOM,
     VISION,
@@ -36,79 +31,30 @@ const std::string HAND_FRAME_NAME = "hand";
 const std::string UNKNOWN_FRAME_NAME = "unknown";
 const std::string RAYCAST_FRAME_NAME = "walkto_raycast_intersection";
 
-class Vector3{
-public:
-    Vector3(double x, double y, double z);
-    double x;
-    double y;
-    double z;
-};
-
-class Quaternion{
-public:
-    Quaternion(double x, double y, double z, double w);
-
-    Quaternion inverse();
-    Quaternion mult(Quaternion otherQuat);
-    Vector3 transformPoint(double x, double y, double z);
-
-    double w();
-    double x();
-    double y();
-    double z();
-
-private:
-    double _w; 
-    double _x;
-    double _y;
-    double _z;
-};
-
-class Pose3{
-public:
-    Pose3(double x, double y, double z, Quaternion quat);
-    Pose3(SE3Pose pose);
-
-    Pose3 mult(Pose3 otherPose);
-    Pose3 inverse();
-
-    double x();
-    double y();
-    double z();
-    Quaternion quat();
-
-private:
-    double _x;
-    double _y;
-    double _z;
-    Quaternion _q;
-};
-
-
 class FrameTree {
 public:
     FrameTree(bosdyn::api::FrameTreeSnapshot snapshot);
 
     class Parent {
     public: 
-        Parent(Pose3 parent_tf_child, std::string parentFrameName);
+        Parent(Math::SE3Pose parent_tf_child, std::string parentFrameName);
 
-        Pose3 parent_tf_child();
+        Math::SE3Pose parent_tf_child();
         std::string parentFrameName();
     private:
-        Pose3 _parent_tf_child;
+        Math::SE3Pose _parent_tf_child;
         std::string _parentFrameName;
     };
 
     std::map<std::string, Parent> childToParentEdges();
 
-    void addEdge(Pose3 parent_tf_child, std::string parentFrame, std::string childFrame);
+    void addEdge(Math::SE3Pose parent_tf_child, std::string parentFrame, std::string childFrame);
 
-    Pose3 a_tf_b(std::string frameA, std::string frameB);
+    Math::SE3Pose a_tf_b(std::string frameA, std::string frameB);
 
 private:
     std::vector<Parent> listParentEdges(std::string leafFrame);
-    Pose3 accumulateTransforms(std::vector<Parent> parents);
+    Math::SE3Pose accumulateTransforms(std::vector<Parent> parents);
 
     std::map<std::string, Parent> _childToParentEdges;
 };
--- src/math.cpp
+++ src/math.cpp
@@ -1,1 +1,68 @@
 #include <spot/math.h>
+
+namespace Math {
+    Vector3::Vector3(double x, double y, double z) :
+            _x(x),
+            _y(y),
+            _z(z) {}
+
+    Vector3::Vector3(bosdyn::api::Vec3 vec) {
+
+    }
+
+    Quaternion::Quaternion(double x, double y, double z, double w) :
+        _x(x),
+        _y(y),
+        _z(z),
+        _w(w) {}
+
+    Quaternion Quaternion::inverse() {
+        return Quaternion(-_x, -_y, -_z, _w);
+    }
+
+    Quaternion Quaternion::operator*(const Quaternion &otherQuat) {
+        return Quaternion( 
+            _w * otherQuat._x + _x * otherQuat._w + _y * otherQuat._z - _z * otherQuat._y,
+            _w * otherQuat._y - _x * otherQuat._z + _y * otherQuat._w + _z * otherQuat._x, 
+            _w * otherQuat._z + _x * otherQuat._y - _y * otherQuat._x + _z * otherQuat._w,
+            _w * otherQuat._w - _x * otherQuat._x - _y * otherQuat._y - _z * otherQuat._z);
+    }
+
+    Vector3 Quaternion::transformPoint(double x, double y, double z) {
+        Quaternion inv = this->inverse();
+        Quaternion q = Quaternion(x, y, z, 0);
+        q = q.mult(inv);
+        q = this->mult(q);
+        return Vector3(q._x, q._y, q._z);
+    }
+
+    SE3Pose::SE3Pose(double x, double y, double z, Quaternion quat) :
+            _x(x),
+            _y(y),
+            _z(z),
+            _q(quat) {}
+
+    SE3Pose SE3Pose::operator*(const SE3Pose &other) {
+        Vector3 vec = _q.transformPoint(other._x, other._y, other._z);
+        return SE3Pose(_x + vec.x, _y + vec.y, _z + vec.z, _q * other._q);
+    }
+
+    SE3Pose SE3Pose::inverse() {
+        Quaternion invQuat = _q.inverse();
+	    Vector3 vec = invQuat.transformPoint(_x, _y, _z);
+	    return SE3Pose(-vec.x, -vec.y, -vec.z, invQuat);
+    }
+
+    SE3Pose SE3Pose::from_identity() {
+        return SE3Pose(0, 0, 0, Quaternion(0, 0, 0, 1));
+    }
+
+    SE3Pose SE3Pose::from_proto(bosdyn::api::SE3Pose pose) {
+        return SE3Pose(
+            pose.position().x(),
+            pose.position().y(),
+            pose.position().z(),
+            Quaternion(pose.rotation().x(), pose.rotation().y(), pose.rotation().z(), pose.rotation().w())
+        );
+    }
+};
