2dac1984619f60f59509496c1258a9b37e7c6e83
Nathaniel Nemenzo <natnemenzo1@gmail.com>
2021-07-15 15:25:31 -0500

    topic renaming, cmdvel changes

--- rospkgs/spot_ros_cpp/src/spot_ros_cpp.cpp
+++ rospkgs/spot_ros_cpp/src/spot_ros_cpp.cpp
@@ -45,7 +45,7 @@ void SpotROSNode::cmd_vel_callback(const geometry_msgs::Twist &msg) {
 	} else {
 		// set time of last nonzero command
 		_t_last_non_zero_vel_cmd = std::chrono::steady_clock::now();
-		_spot.velocityMove(msg.linear.x, msg.linear.y, msg.linear.z, vel_cmd_duration, FLAT_BODY);
+		_spot.velocityMove(msg.linear.x, msg.linear.y, msg.angular.z, vel_cmd_duration, FLAT_BODY);
 	}
 }
 
@@ -77,59 +77,59 @@ void SpotROSNode::run(int argc, char **argv, const std::string &username, const 
 
     /* Publishers */
     // image (handled by image)
-    ros::Publisher image_back_pub = nh.advertise<sensor_msgs::Image>("camera/back/image", 10);
-    ros::Publisher image_frontleft_pub = nh.advertise<sensor_msgs::Image>("camera/frontleft/image", 10);
-    ros::Publisher image_frontright_pub = nh.advertise<sensor_msgs::Image>("camera/frontright/image", 10);
-    ros::Publisher image_left_pub = nh.advertise<sensor_msgs::Image>("camera/left/image", 10);
-    ros::Publisher image_right_pub = nh.advertise<sensor_msgs::Image>("camera/right/image", 10);
+    ros::Publisher image_back_pub = nh.advertise<sensor_msgs::Image>("spot/camera/back/image", 10);
+    ros::Publisher image_frontleft_pub = nh.advertise<sensor_msgs::Image>("spot/camera/frontleft/image", 10);
+    ros::Publisher image_frontright_pub = nh.advertise<sensor_msgs::Image>("spot/camera/frontright/image", 10);
+    ros::Publisher image_left_pub = nh.advertise<sensor_msgs::Image>("spot/camera/left/image", 10);
+    ros::Publisher image_right_pub = nh.advertise<sensor_msgs::Image>("spot/camera/right/image", 10);
 
     // depth
-    ros::Publisher depth_back_pub = nh.advertise<sensor_msgs::Image>("depth/back/image", 10);
-    ros::Publisher depth_frontleft_pub = nh.advertise<sensor_msgs::Image>("depth/frontleft/image", 10);
-    ros::Publisher depth_frontright_pub = nh.advertise<sensor_msgs::Image>("depth/frontright/image", 10);
-    ros::Publisher depth_left_pub = nh.advertise<sensor_msgs::Image>("depth/left/image", 10);
-    ros::Publisher depth_right_pub = nh.advertise<sensor_msgs::Image>("depth/right/image", 10);
+    ros::Publisher depth_back_pub = nh.advertise<sensor_msgs::Image>("spot/depth/back/image", 10);
+    ros::Publisher depth_frontleft_pub = nh.advertise<sensor_msgs::Image>("spot/depth/frontleft/image", 10);
+    ros::Publisher depth_frontright_pub = nh.advertise<sensor_msgs::Image>("spot/depth/frontright/image", 10);
+    ros::Publisher depth_left_pub = nh.advertise<sensor_msgs::Image>("spot/depth/left/image", 10);
+    ros::Publisher depth_right_pub = nh.advertise<sensor_msgs::Image>("spot/depth/right/image", 10);
 
     // image camera info
-    ros::Publisher image_back_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera/back/camera_info", 10);
-    ros::Publisher image_frontleft_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera/frontleft/camera_info", 10);
-    ros::Publisher image_frontright_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera/frontright/camera_info", 10);
-    ros::Publisher image_left_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera/left/camera_info", 10);
-    ros::Publisher image_right_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("camera/right/camera_info", 10);
+    ros::Publisher image_back_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/camera/back/camera_info", 10);
+    ros::Publisher image_frontleft_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/camera/frontleft/camera_info", 10);
+    ros::Publisher image_frontright_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/camera/frontright/camera_info", 10);
+    ros::Publisher image_left_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/camera/left/camera_info", 10);
+    ros::Publisher image_right_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/camera/right/camera_info", 10);
 
     // depth camera info
-    ros::Publisher depth_back_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("depth/back/camera_info", 10);
-    ros::Publisher depth_frontleft_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("depth/frontleft/camera_info", 10);
-    ros::Publisher depth_frontright_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("depth/frontright/camera_info", 10);
-    ros::Publisher depth_left_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("depth/left/camera_info", 10);
-    ros::Publisher depth_right_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("depth/right/camera_info", 10);
+    ros::Publisher depth_back_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/depth/back/camera_info", 10);
+    ros::Publisher depth_frontleft_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/depth/frontleft/camera_info", 10);
+    ros::Publisher depth_frontright_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/depth/frontright/camera_info", 10);
+    ros::Publisher depth_left_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/depth/left/camera_info", 10);
+    ros::Publisher depth_right_camera_info_pub = nh.advertise<sensor_msgs::CameraInfo>("spot/depth/right/camera_info", 10);
 
     // status (handled by robotstate)
-    ros::Publisher joint_state_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 10);
-    ros::Publisher metrics_pub = nh.advertise<spot_msgs::Metrics>("status/metrics", 10);
-    ros::Publisher leases_pub = nh.advertise<spot_msgs::LeaseArray>("status/leases", 10);
-    ros::Publisher odometry_twist_pub = nh.advertise<geometry_msgs::TwistWithCovarianceStamped>("odometry/twist", 10);
-    ros::Publisher odometry_pub = nh.advertise<nav_msgs::Odometry>("odometry", 10);
-    ros::Publisher feet_pub = nh.advertise<spot_msgs::FootStateArray>("status/feet", 10);
-    ros::Publisher estop_pub = nh.advertise<spot_msgs::EStopStateArray>("status/estop", 10);
-    ros::Publisher power_pub = nh.advertise<spot_msgs::PowerState>("status/power_state", 10);
-    ros::Publisher battery_pub = nh.advertise<spot_msgs::BatteryStateArray>("status/battery_states", 10);
+    ros::Publisher joint_state_pub = nh.advertise<sensor_msgs::JointState>("spot/joint_states", 10);
+    ros::Publisher metrics_pub = nh.advertise<spot_msgs::Metrics>("spot/status/metrics", 10);
+    ros::Publisher leases_pub = nh.advertise<spot_msgs::LeaseArray>("spot/status/leases", 10);
+    ros::Publisher odometry_twist_pub = nh.advertise<geometry_msgs::TwistWithCovarianceStamped>("spot/odom/twist", 10);
+    ros::Publisher odometry_pub = nh.advertise<nav_msgs::Odometry>("spot/odom", 10);
+    ros::Publisher feet_pub = nh.advertise<spot_msgs::FootStateArray>("spot/status/feet", 10);
+    ros::Publisher estop_pub = nh.advertise<spot_msgs::EStopStateArray>("spot/status/estop", 10);
+    ros::Publisher power_pub = nh.advertise<spot_msgs::PowerState>("spot/status/power_state", 10);
+    ros::Publisher battery_pub = nh.advertise<spot_msgs::BatteryStateArray>("spot/status/battery_states", 10);
 
     // feedback (handled by robot id, sdk)
-    ros::Publisher feedback_pub = nh.advertise<spot_msgs::Feedback>("status/feedback", 10);
+    ros::Publisher feedback_pub = nh.advertise<spot_msgs::Feedback>("spot/status/feedback", 10);
 
     // mobility params (handled by sdk)
-    ros::Publisher mobility_params_pub = nh.advertise<spot_msgs::MobilityParams>("status/mobility_params", 10);
+    ros::Publisher mobility_params_pub = nh.advertise<spot_msgs::MobilityParams>("spot/status/mobility_params", 10);
 
     /* Subscribers TODO: define callbacks*/
     ros::Subscriber cmd_vel_sub = nh.subscribe("cmd_vel", 10, &SpotROSNode::cmd_vel_callback, this);
     // ros::Subscriber body_pose_sub = nh.subscribe("body_pose");
 
     /* Services TODO: callbacks */
-    ros::ServiceServer sit_srv = nh.advertiseService("sit", &SpotROSNode::sit_srv, this);
-    ros::ServiceServer stand_srv = nh.advertiseService("stand", &SpotROSNode::stand_srv, this);
-    ros::ServiceServer power_on_srv = nh.advertiseService("power_on", &SpotROSNode::power_on_srv, this);
-    ros::ServiceServer power_off_srv = nh.advertiseService("power_off", &SpotROSNode::power_off_srv, this);
+    ros::ServiceServer sit_srv = nh.advertiseService("spot/sit", &SpotROSNode::sit_srv, this);
+    ros::ServiceServer stand_srv = nh.advertiseService("spot/stand", &SpotROSNode::stand_srv, this);
+    ros::ServiceServer power_on_srv = nh.advertiseService("spot/power_on", &SpotROSNode::power_on_srv, this);
+    ros::ServiceServer power_off_srv = nh.advertiseService("spot/power_off", &SpotROSNode::power_off_srv, this);
     // ros::ServiceServer stair_mode_srv = nh.advertiseService("stair_mode");
     // ros::ServiceServer locomotion_mode_srv = nh.advertiseService("locomotion_mode");
 
@@ -227,7 +227,7 @@ void SpotROSNode::run(int argc, char **argv, const std::string &username, const 
 
             // odometry (twist and regular)
             odometry_twist_pub.publish(get_odom_twist_from_state(state));
-            odometry_pub.publish(get_odom_from_state(state));
+            odometry_pub.publish(get_odom_from_state(state, false));
 
             // feet
             feet_pub.publish(get_foot_states_from_robotstate(state));
@@ -275,6 +275,7 @@ void SpotROSNode::run(int argc, char **argv, const std::string &username, const 
 
     // sit and power off
     _spot.sit();
+    sleep(5);
     _spot.powerOffBlocking();
 }
 
